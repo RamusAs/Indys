7bcd155712e53993f68a9a4dc397de60fd4e635d
Valentin Afonso <valentinafonso22@gmail.com>
2023-06-22 14:50:29 +0200

    add navigation

--- js/tabs.js
+++ js/tabs.js
@@ -1,31 +1,87 @@
 window.onload = function () {
+  checkItemsActive();
   const items = document.querySelectorAll(".item");
   const tabs = document.querySelectorAll(".tab");
-  let anyTabActive = false;
+
   for (let i = 0; i < tabs.length; i++) {
     tabs[i].addEventListener("click", () => {
+      checkItemsActive();
       for (let j = 0; j < items.length; j++) {
         if (j === i) {
           items[j].classList.add("active");
           tabs[j].classList.add("active");
-          anyTabActive = true;
+          displayDefaultPage();
         } else {
           items[j].classList.remove("active");
           tabs[j].classList.remove("active");
         }
       }
 
-      if (!anyTabActive) {
-        console.log("ok");
-        const pages = document.querySelectorAll(".page");
-        for (let k = 0; k < pages.length; k++) {
-          if (pages[k].id !== "page-default") {
-            pages[k].classList.remove("active");
-          }
-        }
-      }
-
-      anyTabActive = false;
     });
   }
+  
+    items.forEach(item => {
+      const itemsButtons = item.querySelectorAll("button");
+      itemsButtons.forEach(button => {
+        button.addEventListener("click", (e) => {
+          itemsButtons.forEach(btn => {
+            btn.classList.remove("active");
+          });
+          button.classList.add("active");
+          checkItemsActive();
+          getPage();
+        });
+      });
+    });
+
+    function checkItemsActive() {
+      const items = document.querySelectorAll(".item");
+      items.forEach(item => {
+        const itemsButtons = item.querySelectorAll("button");
+        const hasActiveButton = Array.from(itemsButtons).some(btn => {
+          return btn.classList.contains("active");
+        });
+    
+        if (hasActiveButton) {
+          item.classList.remove("empty");
+        } else {
+          item.classList.add("empty");
+        }
+      });
+      getPage();
+      displayDefaultPage();
+    }
+
+    function displayDefaultPage() {
+      if (document.querySelector(".item.active").classList.contains("empty")) {
+        document.querySelector("#page-default").classList.add("active");
+      } else {
+        document.querySelector("#page-default").classList.remove("active");
+
+      }
+    }
+
+    function getPage() {
+      const buttonRead = document.querySelector(".item.active button.read");
+      const buttonSummarize = document.querySelector(".item.active button.summarize");
+     
+      if (buttonRead.classList.contains("active")) {
+        document.querySelector("#page-read").classList.add("active");
+        document.querySelector("#page-summarize").classList.remove("active");
+        document.querySelector("#page-default").classList.remove("active");
+      } else {
+        document.querySelector("#page-read").classList.remove("active");
+      }
+      if (buttonSummarize.classList.contains("active")) {
+        document.querySelector("#page-summarize").classList.add("active");
+        document.querySelector("#page-default").classList.remove("active");
+        document.querySelector("#page-read").classList.remove("active");
+      } else {
+        document.querySelector("#page-summarize").classList.remove("active");
+      }
+
+    }
+
+
+
 }
